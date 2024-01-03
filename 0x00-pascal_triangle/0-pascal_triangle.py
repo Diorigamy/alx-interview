0b2a5b3159f79d371c90291b27db7af679428e88
diorigamy <wanlewislegend@gmail.com>
2024-01-03 03:35:27 -0800

    /_\

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,28 @@
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row and print it.
+
+    Args:
+    - n: An integer representing the number of rows in Pascal's Triangle.
+
+    Returns:
+    - A list of lists of integers representing Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
+
+    # Print the generated triangle
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+    return triangle
+
